7309968af50ffbe3f5171cdc022963504945b86f
Neil <nkurien97@gmail.com>
2022-01-16 14:41:52 +0530

    header styling done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,22 @@
 import React from "react";
 
+import logoType from "../img/todo-logo.svg";
+
 const Header = () => {
 	return (
-		<>
-			<div className="title">
-				<img src="" alt="to do logotype" />
+		<div className="header-container">
+			<div className="header-title">
+				<img src={logoType} alt="to do logotype" />
 				<h1>List</h1>
 			</div>
-			<div className="separator"></div>
+			<div className="header-separator"></div>
 			<div className="subtitle">
-				<p>
+				<h4>
 					As with everyone learning React, my time had come to build a
 					to-do list.
-				</p>
+				</h4>
 			</div>
-		</>
+		</div>
 	);
 };
 

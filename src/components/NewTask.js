eb1ce44f281c3517795889c1d22760f6c7043e22
Neil <nkurien97@gmail.com>
2022-02-06 18:58:59 +0530

    added more input styling and logic

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,27 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+import { v4 as uuidv4 } from "uuid";
 
 const NewTask = ({ priority, task, tasks, setTasks, id }) => {
+	//State
+	const [textInput, setTextInput] = useState("");
+	const [isEnabled, setIsEnabled] = useState(() => {
+		return textInput !== "";
+	});
+
+	//Disable buttons if there is no text entered
+	useEffect(() => {
+		textInput !== "" ? setIsEnabled(true) : setIsEnabled(false);
+	}, [textInput]);
+
 	//Input Handlers
-	const updateTasksHandler = (e) => {
-		const newTasks = tasks.map((t) => {
-			if (e.target.id === t.id) {
-				return {
-					...t,
-					isDone: !t.isDone,
-				};
-			} else {
-				return {
-					...t,
-				};
-			}
-		});
-		setTasks(newTasks);
+
+	const updateTextValue = (e) => {
+		setTextInput(e.target.value);
+	};
+
+	const addTaskHandler = (e) => {
+		e.preventDefault();
+
+		const newTask = {
+			taskDetail: textInput,
+			priority: priority,
+			id: uuidv4(),
+			isDone: false,
+		};
+
+		setTasks([...tasks, newTask]);
+		//clear out textInput
+		setTextInput("");
+
+		//write cancel function
+		//hide add task input
+		//make sure task is entering from the top
+		//animate entry of task
 	};
 
 	return (
 		<div className="task-wrapper">
-			<div className={`new-task ${priority}`} id={id}>
+			<form className={`new-task ${priority}`} id={id}>
 				<textarea
+					placeholder="What do you want to do?"
+					value={textInput}
+					onChange={updateTextValue}
 					type="text"
 					className={`new-task-input ${priority}`}
 				/>
@@ -29,11 +54,23 @@ const NewTask = ({ priority, task, tasks, setTasks, id }) => {
 					<button className={`${priority}-secondary-button`}>
 						Cancel
 					</button>
-					<button className={`${priority}-primary-button`}>
+					<button
+						className={
+							isEnabled
+								? `${priority}-primary-button`
+								: `${priority}-disabled-button`
+						}
+						onClick={
+							isEnabled
+								? addTaskHandler
+								: console.log(`errorState ${priority}`)
+						}
+						type="submit"
+					>
 						Add Task
 					</button>
 				</div>
-			</div>
+			</form>
 		</div>
 	);
 };
